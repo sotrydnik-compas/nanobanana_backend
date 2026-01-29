0a90eeb220040a7d6c7daacef7bf5c416fd2f53a
Nikita Vyskubov <vysnik@gmail.com>
2026-01-29 10:28:38 +0300

    исправил middleware для работы с iframe

--- core/middleware.py
+++ core/middleware.py
@@ -3,8 +3,8 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class AllowIframeMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        # Разрешить встраивание в iframe
-        response.headers["X-Frame-Options"] = "ALLOWALL"
-        # Если где-то включён CSP — добавь frame-ancestors
+
+        response.headers.pop("X-Frame-Options", None)
         response.headers["Content-Security-Policy"] = "frame-ancestors *"
+
         return response
