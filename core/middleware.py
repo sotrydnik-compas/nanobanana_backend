1d3c950232821a284e7dab9b410f6e3b60c46021
Nikita Vyskubov <vysnik@gmail.com>
2026-01-29 10:41:48 +0300

    исправил middleware для работы с iframe

--- core/middleware.py
+++ core/middleware.py
@@ -4,7 +4,9 @@ class AllowIframeMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
 
-        response.headers.pop("X-Frame-Options", None)
+        if "X-Frame-Options" in response.headers:
+            del response.headers["X-Frame-Options"]
+
         response.headers["Content-Security-Policy"] = "frame-ancestors *"
 
         return response
